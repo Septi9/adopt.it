06704c846a9def16add7a22c2a2ef5a7a7623520
Monika Zając <monzaj19@wp.pl>
2022-03-15 13:02:48 +0100

    Dockerfile working

--- Back-end/src/main/java/Application/Main.java
+++ Back-end/src/main/java/Application/Main.java
@@ -5,8 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main implements CommandLineRunner {
 
     @Autowired
@@ -14,6 +17,11 @@ public class Main implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("adoptit-image")
+    public String get() {
+        return "Siemanko!";
     }
 
     @Override
