98dba2b8cee26cfc1b30e2b009809fad7595f945
Monika Zając <monzaj19@wp.pl>
2022-03-22 10:26:26 +0100

    check update

--- Back-end/src/main/java/Application/Main.java
+++ Back-end/src/main/java/Application/Main.java
@@ -21,7 +21,7 @@ public class Main implements CommandLineRunner {
 
     @GetMapping("/")
     public String getFirst() {
-        return "Cześć!!!";
+        return "Witamy na stronie głównej!";
     }
 
     @GetMapping("adoptit-image")
