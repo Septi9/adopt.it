8a1563866a3b69b2b0218ec37e4ec49696db007f
Kamil Rozwałka <rozwalka.kamil1@gmail.com>
2022-03-29 13:17:34 +0200

    frontend register and login step 1

---
+++ frontend/adoptit-front/src/app/registration.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {User} from "./user";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+
+  constructor( private _http : HttpClient) { }
+
+  public loginUserFromRemote(user :User):Observable<any> {
+    return this._http.post<any>("http://localhost:8080/login", user)
+  }
+}
