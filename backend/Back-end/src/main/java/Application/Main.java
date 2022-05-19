710edcdb4deb87d7090e07c3b7a054e33a5597d4
Monika Zając <monzaj19@wp.pl>
2022-05-19 10:25:18 +0200

    update main - test docker push

--- backend/Back-end/src/main/java/Application/Main.java
+++ backend/Back-end/src/main/java/Application/Main.java
@@ -21,7 +21,7 @@ public class Main implements CommandLineRunner {
 
     @GetMapping("/")
     public String getFirst() {
-        return "Witamy ponownie w ADOPT.IT!";
+        return "Strona ADOPT.IT!";
     }
 
 
