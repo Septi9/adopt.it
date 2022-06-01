ff5d08418b0469e96f92e5ba18ae52ba6fb000a2
Monika Zając <monzaj19@wp.pl>
2022-06-01 14:04:48 +0200

    update

--- backend/Back-end/src/test/java/RegisterTest.java
+++ backend/Back-end/src/test/java/RegisterTest.java
@@ -19,8 +19,8 @@ public class RegisterTest {
 //        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/");
 
 //        WebDriverManager.chromedriver().setup();
-//        System.setProperty("webdriver.gecko.driver","src/test/resources/driver/geckodriver.exe");
-        System.setProperty("webdriver.gecko.driver","src/test/resources/CIdriver/geckodriver");
+        System.setProperty("webdriver.gecko.driver","src/test/resources/driver/geckodriver.exe");
+//        System.setProperty("webdriver.gecko.driver","src/test/resources/CIdriver/geckodriver");
         webDriver = new FirefoxDriver();
         webDriver.navigate().to("https://adoptit-site.azurewebsites.net/");
 
